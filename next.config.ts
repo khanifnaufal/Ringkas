7c0322dca26638e1a132852de6a699158c4e6d65
khanifnaufal <mkhanif86@gmail.com>
2026-06-04 18:37:41 +0700

    fixing vercel builds

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["pdfjs-dist"],
+  turbopack: {},
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
   webpack: (config, { dev }) => {
     if (dev) {
       config.cache = false;
